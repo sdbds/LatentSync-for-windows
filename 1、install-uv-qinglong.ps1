a30e918f8db1c41e328d096d9f070b2a1bea9368
sdbds <865105819@qq.com>
2025-01-07 14:13:25 +0800

    update for inference model

--- 1、install-uv-qinglong.ps1
+++ 1、install-uv-qinglong.ps1
@@ -125,7 +125,13 @@ if ($download_hy -eq "y" -or $download_hy -eq "Y") {
     Move-Item -Path "$PSScriptRoot/checkpoints/auxiliary/vgg16-397923af.pth" -Destination "torch/hub/checkpoints/vgg16-397923af.pth" -Force
 }
 else {
-    huggingface-cli download chunyu-li/LatentSync --local-dir checkpoints --include "*latentsync_unet.pt" "*tiny.pt"
+    huggingface-cli download chunyu-li/LatentSync --local-dir checkpoints --include "*latentsync_unet.pt" "*tiny.pt" "*s3fd-619a316812.pth" "*2DFAN4-cd938726ad.zip"
+
+    # Create torch directory and its subdirectories
+    New-Item -ItemType Directory -Force -Path "torch/hub/checkpoints" | Out-Null
+
+    Move-Item -Path "$PSScriptRoot/checkpoints/auxiliary/2DFAN4-cd938726ad.zip" -Destination "torch/hub/checkpoints/2DFAN4-cd938726ad.zip" -Force
+    Move-Item -Path "$PSScriptRoot/checkpoints/auxiliary/s3fd-619a316812.pth" -Destination "torch/hub/checkpoints/s3fd-619a316812.pth" -Force
 }
 
 Write-Output "Install finished"
